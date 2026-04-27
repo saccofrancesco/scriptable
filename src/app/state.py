c67a1d0bffd4e2b05d682211c5f739e135c93839
Francesco Sacco <francescosacco.github@gmail.com>
2026-04-27 19:08:59 +0200

    feat(state): add function to create default view state

--- src/app/state.py
+++ src/app/state.py
@@ -8,3 +8,8 @@ from ..domain.models import Schedule
 class ViewState:
     selected_month: date
     selected_day: date
+
+
+def create_default_view_state(today: date | None = None) -> ViewState:
+    today: date | None = today or date.today()
+    return ViewState(selected_month=today.replace(day=1), selected_day=today)
