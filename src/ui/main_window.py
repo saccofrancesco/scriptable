ac10d2c3cefe750e4b59726ad0cc65e3a63c029e
Francesco Sacco <francescosacco.github@gmail.com>
2026-04-28 15:56:07 +0200

    feat(ui): add employee edit functionality

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -63,6 +63,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central)
 
         self.employee_panel.add_requested.connect(self._add_employee)
+        self.employee_panel.edit_requested.connect(self._edit_employee)
         self.controller.changed.connect(self.refresh)
 
         self.refresh()
